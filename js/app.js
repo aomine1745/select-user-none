9b97ccc7776d3594641d50435379130b6c5970cc
aomine <xomarxpl07@outlook.es>
2019-03-11 13:39:52 -0500

    v 1.0

---
+++ js/app.js
@@ -0,0 +1,5 @@
+        var icon = document.querySelector('.icon');
+        icon.addEventListener('click', toogle);
+        function toogle(){
+        	this.classList.toggle('active');
+        }
